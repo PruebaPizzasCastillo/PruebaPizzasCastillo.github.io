5dfe1d1fe0652b444f82b48a131e9e6318b60264
PruebaPizzasCastillo <PruebaPizzasCastillo@gmail.com>
2021-12-10 20:29:49 -0600

    subida de init

--- js/init.js
+++ js/init.js
@@ -1,9 +1,9 @@
 firebase.initializeApp({
-  apiKey: "AIzaSyBb4rhWfiyy478hk1ulGawXZG9e5wtQd0w",
-  authDomain: "pizzascastillo-ff02c.firebaseapp.com",
-  projectId: "pizzascastillo-ff02c",
-  storageBucket: "pizzascastillo-ff02c.appspot.com",
-  messagingSenderId: "334670760409",
-  appId: "1:334670760409:web:05ae16bc1c3ef4463471c8",
-  measurementId: "${config.measurementId}"
+    apiKey: "AIzaSyA2RWGgpSSEczlikYnJ3sNxB5KgGRh3Ueo",
+    authDomain: "pruebapizzascastillo-f8ce5.firebaseapp.com",
+    projectId: "pruebapizzascastillo-f8ce5",
+    storageBucket: "pruebapizzascastillo-f8ce5.appspot.com",
+    messagingSenderId: "167454275761",
+    appId: "1:167454275761:web:bc74a145b9a62d36668a90",
+    measurementId: "${config.measurementId}"
 });
