a66a9e8776920898636993458c7859968b8cfb90
PruebaPizzasCastillo <PruebaPizzasCastillo@gmail.com>
2021-12-14 18:21:12 -0600

    ctrlEditar

--- js/CtrlEditar.js
+++ js/CtrlEditar.js
@@ -26,31 +26,66 @@ onAuthStateChanged(
     "../lib/tiposFire.js").User}
     usuario */
 async function protege(usuario) {
-    if (tieneRol(usuario, ["Administrador"])) {
+    if (tieneRol(usuario, ["Cliente"])) {
         consulta();
     }
 }
 
+/** Muestra los mensajes
+ * almacenados en la collection
+ * "Alimento". Se actualiza
+ * automáticamente. */
 function consulta() {
+    /* Consulta que se actualiza
+     * automáticamente. Pide todos
+     * los registros de la colección
+     *  "Alimento"
+     * ordenados por el campo
+     *  "idProducto"
+     * de forma
+     *  descendente. */
     daoAlimento.
-    orderBy("idProducto")
-        .onSnapshot(
-            htmlLista, errConsulta);
+    orderBy("idProducto").
+    onSnapshot(
+        htmlLista, errConsulta);
 }
 
-/**
+/** Muestra los datos enviados por
+ * el servidor.
+ * Si los datos cambian en el
+ * servidor, se vuelve a invocar
+ * esta función y recibe los datos
+ * actualizados.
  * @param {import(
     "../lib/tiposFire.js").
-    QuerySnapshot} snap */
+    QuerySnapshot} snap estructura
+ *    parecida a un Array, que
+ *    contiene una copia de los
+ *    datos del servidor.
+ */
 function htmlLista(snap) {
     let html = "";
     if (snap.size > 0) {
+        /* Cuando el número de
+         * documentos devueltos por la
+         * consulta es mayor que 0,
+         * revisa uno por uno los
+         * documentos de la consulta y
+         * los muestra. El iterador
+         * "doc" apunta a un
+         * documento de la base
+         * de datos. */
         snap.forEach(doc =>
             html += htmlFila(doc));
     } else {
+        /* Cuando el número de
+         * documentos devueltos por la
+         * consulta es igual a 0,
+         * agrega un texto HTML. */
         html += /* html */
             `<li class="vacio">
-          -- No hay alimentos. --
+          -- No hay platillos
+          registrados. --
         </li>`;
     }
     lista.innerHTML = html;
@@ -67,24 +102,38 @@ function htmlFila(doc) {
     const data = doc.data();
     const idProducto = cod(data.idProducto);
     const nombre = cod(data.nombre);
-    const costo = cod(data.costo);
+    const precio = cod(data.precio);
     const descripcion = cod(data.descripcion);
     const parámetros = new URLSearchParams();
-    parámetros.append("idProducto", doc.idProducto);
+    parámetros.append("id", doc.id);
     return ( /* html */
-        `<li>
-        <a class="fila" href=
-    "menu.html?${parámetros}">
-          <strong class="primario">
-            ${idProducto} // ${nombre} // ${costo} // ${descripcion}
-          </strong>
+        `<li class="fila" style="padding: 2em">
+        <a href="alimentoNuevo.html?${parámetros}">
+        <strong class="primario" style="padding: 2em">
+          <br>
+          ${cod(data.nombre)}
+          <br>
+          ${cod(data.costo)}
+          <br>
+          <p>
+          Descripción: <br>
+          ${cod(data.descripcion)}
+          </p>
+        </strong>
         </a>
-       
       </li>`);
 }
 
-/** @param {Error} e */
+/** Función que se invoca cuando
+ * hay un error al recuperar los
+ * mensajes y muestra el error. Al
+ * invocar esta función, la
+ * conexión se cancela, por lo
+ * cual intenta conectarse otra
+ * vez.
+ * @param {Error} e */
 function errConsulta(e) {
     muestraError(e);
+    // Intenta conectarse otra vez.
     consulta();
 }
